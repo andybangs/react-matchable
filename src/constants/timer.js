37f580863b6db64b1d713fff120c64971ee407f2
Andy Bangs <andrew.yuta@gmail.com>
2016-02-14 22:43:10 -0800

    handle RESET_STATE

--- src/constants/timer.js
+++ src/constants/timer.js
@@ -1,1 +1,2 @@
 export const TICK_TIMER = 'TICK_TIMER';
+export const RESET_STATE = 'RESET_STATE';
--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -1,4 +1,4 @@
-import { TICK_TIMER } from '../constants/timer';
+import { TICK_TIMER, RESET_STATE } from '../constants/timer';
 
 const initialState = {
   seconds: 90,
@@ -8,6 +8,9 @@ export default function timer(state = initialState, action) {
   switch (action.type) {
     case TICK_TIMER:
       return { ...state, seconds: state.seconds - 1 };
+
+    case RESET_STATE:
+      return initialState;
 
     default:
       return state;
