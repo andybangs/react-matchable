b482314227a59596bbeb5b52ef4db3494c9c61fe
Andy Bangs <andrew.yuta@gmail.com>
2016-03-15 18:59:07 -0700

    fix bg color flicker bug

--- src/components/QuizBody.js
+++ src/components/QuizBody.js
@@ -4,6 +4,80 @@ import Radium from 'radium';
 import { flatten, head } from 'lodash';
 import { PLAYING, END } from '../constants/gameStates';
 
+// buildPlayItem :: Item -> Bool -> [Item.mid, Item.id] -> Func -> JSX
+function buildPlayItem(item, isMatched, attemptedItem, action) {
+  const matched = isMatched ? 'matchedAnimation' : '';
+  const missed = head(attemptedItem) === item.mid ? 'missedAnimation' : '';
+  const selected = item.selected ? 'selectedColor' : '';
+
+  function handleClick() {
+    action(item.mid, item.id);
+  }
+
+  return (
+    <li key={`${item.mid}-${item.id}`}
+      onClick={handleClick}
+      className={missed}
+      style={[
+        styles.li,
+        styles[selected],
+        styles[matched],
+        styles[missed],
+      ]}
+    >{item.value}</li>
+  );
+}
+
+// buildEndItem :: Item -> Bool -> Bool -> Func -> JSX
+function buildEndItem(item, isMatched, isFocused, action) {
+  const color = isMatched ? 'matchedColor' : 'missedColor';
+  const selected = isFocused ? 'selectedColor' : '';
+
+  function handleOnMouseOver() {
+    action(item.mid);
+  }
+
+  function handleOnMouseLeave() {
+    action(null);
+  }
+
+  return (
+    <li key={`${item.mid}-${item.id}`}
+      onMouseEnter={handleOnMouseOver}
+      onMouseLeave={handleOnMouseLeave}
+      style={[
+        styles.li,
+        styles[color],
+        styles[selected],
+      ]}
+    >{item.value}</li>
+  );
+}
+
+// buildColumn :: Array Matchable -> Number -> [Item.mid, Item.id] -> String -> Func -> JSX
+function buildColumn(columnArr, columnKey, attemptedItem, gameState, action) {
+  switch (gameState) {
+    case PLAYING:
+      const playItems = columnArr.map(m => {
+        const i = head(m.items);
+        return buildPlayItem(i, m.matched, attemptedItem, action);
+      });
+
+      return <ul key={columnKey} style={styles.ul}>{playItems}</ul>;
+
+    case END:
+      const endItems = columnArr.map(m => {
+        const i = head(m.items);
+        return buildEndItem(i, m.matched, i.focused, action);
+      });
+
+      return <ul key={columnKey} style={styles.ul}>{endItems}</ul>;
+
+    default:
+      return console.error('gameState must be PLAYING or END');
+  }
+}
+
 const QuizBody = (props) => {
   const {
     gameState,
@@ -13,79 +87,6 @@ const QuizBody = (props) => {
     attempted,
   } = props;
 
-  // buildPlayItem :: Item -> Bool -> [Item.mid, Item.id] -> JSX
-  function buildPlayItem(item, isMatched, attemptedItem) {
-    const matched = isMatched ? 'matchedAnimation' : '';
-    const missed = head(attemptedItem) === item.mid ? 'missedAnimation' : '';
-    const selected = item.selected ? 'selected' : '';
-
-    function handleClick() {
-      action(item.mid, item.id);
-    }
-
-    return (
-      <li key={`${item.mid}-${item.id}`}
-        onClick={handleClick}
-        style={[
-          styles.li,
-          styles[selected],
-          styles[matched],
-          styles[missed],
-        ]}
-      >{item.value}</li>
-    );
-  }
-
-  // buildEndItem :: Item -> Bool -> Bool -> JSX
-  function buildEndItem(item, isMatched, isFocused) {
-    const color = isMatched ? 'matchedColor' : 'missedColor';
-    const selected = isFocused ? 'selected' : '';
-
-    function handleOnMouseOver() {
-      action(item.mid);
-    }
-
-    function handleOnMouseLeave() {
-      action(null);
-    }
-
-    return (
-      <li key={`${item.mid}-${item.id}`}
-        onMouseEnter={handleOnMouseOver}
-        onMouseLeave={handleOnMouseLeave}
-        style={[
-          styles.li,
-          styles[color],
-          styles[selected],
-        ]}
-      >{item.value}</li>
-    );
-  }
-
-  // buildColumn :: Array Matchable -> Number -> [Item.mid, Item.id] -> JSX
-  function buildColumn(columnArr, columnKey, attemptedItem) {
-    switch (gameState) {
-      case PLAYING:
-        const playItems = columnArr.map(m => {
-          const i = head(m.items);
-          return buildPlayItem(i, m.matched, attemptedItem);
-        });
-
-        return <ul key={columnKey} style={styles.ul}>{playItems}</ul>;
-
-      case END:
-        const endItems = columnArr.map(m => {
-          const i = head(m.items);
-          return buildEndItem(i, m.matched, i.focused);
-        });
-
-        return <ul key={columnKey} style={styles.ul}>{endItems}</ul>;
-
-      default:
-        return console.error('gameState must be PLAYING or END');
-    }
-  }
-
   // columns :: Array JSX
   const columnsArr = itemIds.map(id => {
     // attempt :: [Item.mid, Item.id] || []
@@ -93,7 +94,7 @@ const QuizBody = (props) => {
       flatten(attempted.filter(arr => arr[1] === id)) :
       [];
 
-    return buildColumn(columns[id], id, attempt);
+    return buildColumn(columns[id], id, attempt, gameState, action);
   });
 
   return (
@@ -102,9 +103,7 @@ const QuizBody = (props) => {
       transitionAppear={true}
       transitionAppearTimeout={500}
     >
-      <div style={styles.container}>
-          {columnsArr}
-      </div>
+      <div style={styles.container}>{columnsArr}</div>
     </ReactCSSTransitionGroup>
   );
 };
@@ -146,35 +145,36 @@ const styles = {
     margin: 4,
     padding: 5,
     borderRadius: 2,
-    fontSize: '0.9em',
     textAlign: 'center',
     cursor: 'pointer',
     ':active': {
       boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
     },
   },
-  selected: {
+  selectedColor: {
     backgroundColor: '#fcffbd',
-  },
-  matchedAnimation: {
-    backgroundColor: '#9ff5a9',
-    boxShadow: '',
-    animationName: matchedKeyframes,
-    animationDuration: '300ms',
-    animationIterationCount: 1,
-    animationTimingFunction: 'linear',
-  },
-  missedAnimation: {
-    animationName: missedKeyframes,
-    animationDuration: '300ms',
-    animationIterationCount: 1,
-    animationTimingFunction: 'linear',
   },
   matchedColor: {
     backgroundColor: '#9ff5a9',
+    boxShadow: '',
   },
   missedColor: {
     backgroundColor: '#ff9684',
+    boxShadow: '',
+  },
+  matchedAnimation: {
+    boxShadow: '',
+    animationName: matchedKeyframes,
+    animationDuration: '400ms',
+    animationIterationCount: 1,
+    animationTimingFunction: 'linear',
+    animationFillMode: 'forwards',
+  },
+  missedAnimation: {
+    animationName: missedKeyframes,
+    animationDuration: '400ms',
+    animationIterationCount: 1,
+    animationTimingFunction: 'linear',
   },
 };
 
